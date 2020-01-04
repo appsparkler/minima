ce42c19437c756d5991f093e270fa3254c7bfcb4
Akash <subs@appsparkler.com>
2020-01-04 10:08:56 +0530

    working webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
-      path: __dirname + '/assets',
-      filename: 'js/[name].[contenthash:8].js',
+      path: __dirname + '/assets/js',
+      filename: '[name].[contenthash:8].js',
       publicPath: '/assets/'
 
     },
     plugins: [
+      new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: false,
+        protectWebpackAssets: true
+      }),
       new HtmlWebpackPlugin({
         template: './src/index.ejs',
-        filename: '../_includes/webpacked-libs.html'
+        filename: '../../_includes/webpacked-libs.html'
       })
     ]
 }
